c36d9934b1cf158e130f7171548f1b78aed1e1ef
Andre C. Scherrer <andrescherrer@gmail.com>
2025-12-21 21:18:36 -0300

    chore: BE-002 - add dependencies to go.mod (Fiber, GORM, validator, JWT, zerolog, uuid, bcrypt)

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	_ "github.com/gofiber/fiber/v2"
+	_ "github.com/golang-jwt/jwt/v5"
+	_ "gorm.io/gorm"
+	_ "gorm.io/driver/postgres"
+	_ "github.com/go-playground/validator/v10"
+	_ "github.com/rs/zerolog"
+	_ "github.com/google/uuid"
+	_ "golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	// TODO: Implement main function
+}
+
